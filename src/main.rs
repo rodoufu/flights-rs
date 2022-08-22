d3db722554f738a2d120b9ecdb3239476df9f4ac
Rodolfo Araujo <rodoufu@gmail.com>
2022-08-22 13:47:26 -0300

    Selecting port.

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,12 @@ async fn handle_flight(req: web::Json<FlightRequest>) -> web::Json<FlightRespons
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port = std::env::var("PORT").unwrap_or("8080".to_string());
+    let port = port.parse::<u16>().expect(&format!("invalid port value: {}", port));
+
     HttpServer::new(|| App::new().service(handle_flight))
         .workers(4)
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", port))?
         .run()
         .await
 }
