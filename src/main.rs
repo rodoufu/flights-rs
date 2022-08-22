6fee89180333fdb58d3eec1f998079eccc566970
Rodolfo Araujo <rodoufu@gmail.com>
2022-08-22 15:05:31 -0300

    Adding metrics.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,39 @@ mod flight;
 
 use actix_web::{
     App,
+    get,
+    HttpResponse,
     HttpServer,
     middleware::Logger,
     post,
     web,
+};
+use actix_web_prom::{
+    PrometheusMetrics,
+    PrometheusMetricsBuilder,
 };
 use crate::flight::{
     FlightRequest,
     FlightResponse,
 };
 use log::info;
+use prometheus::{
+    opts,
+    IntCounterVec,
+};
+use std::collections::HashMap;
+
+#[get("/health")]
+async fn health() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
 #[post("/flight")]
-async fn handle_flight(req: web::Json<FlightRequest>) -> web::Json<FlightResponse> {
+async fn handle_flight(
+    req: web::Json<FlightRequest>, counter: web::Data<IntCounterVec>,
+) -> web::Json<FlightResponse> {
     info!("got a new request");
+    counter.with_label_values(&["endpoint", "method", "status"]).inc();
     web::Json(FlightResponse::Error { message: "not implemented yet".to_string() })
 }
 
@@ -28,8 +47,28 @@ async fn main() -> std::io::Result<()> {
     info!("using port: {}", port);
     let port = port.parse::<u16>().expect(&format!("invalid port value: {}", port));
 
-    HttpServer::new(
-        || App::new().wrap(Logger::new("%a %{User-Agent}i")).service(handle_flight)
+    let flights_counter_opts = opts!("flight_requests", "number of flight requests")
+        .namespace("flights");
+    let flights_counter = IntCounterVec::new(
+        flights_counter_opts, &["endpoint", "method", "status"],
+    ).expect("problem creating flights counter");
+
+    let prometheus = PrometheusMetricsBuilder::new("api")
+        .endpoint("/metrics")
+        .build()
+        .expect("problem creating prometheus builder");
+
+    prometheus
+        .registry
+        .register(Box::new(flights_counter.clone()))
+        .unwrap();
+
+    HttpServer::new(move ||
+        App::new()
+            .wrap(prometheus.clone())
+            .data(flights_counter.clone())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .service(handle_flight)
     ).workers(4)
         .bind(("127.0.0.1", port))?
         .run()
